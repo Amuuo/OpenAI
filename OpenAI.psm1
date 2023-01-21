f71b933272edae5419ac6d1e96e8af495d8b6a11
adam <adamthewilliams@gmail.com>
2023-01-21 14:35:46 -0600

    Added temperature to the request body

--- OpenAI.psm1
+++ OpenAI.psm1
@@ -2,11 +2,11 @@ function Invoke-TextCompletion {
 
     [CmdletBinding()]
     Param(
-        [string]$Prompt,
-        [string]$Temperature,
-        [string]$ApiKey = "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",
-        [string]$Model = 'text-davinci-003',
-        [Int32]$MaxTokens = 4000
+        $Prompt      = 'Say this is a test',
+        $Temperature = 0.0,
+        $ApiKey      = "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",
+        $Model       = 'text-davinci-003',
+        $MaxTokens   = 4000
     )
     
     $restMethodParams = @{
@@ -14,6 +14,7 @@ function Invoke-TextCompletion {
         Uri     = "https://api.openai.com/v1/engines/$Model/completions"
         Body    = @{
                         prompt = $Prompt
+                        temperature = $Temperature
                         max_tokens = $MaxTokens
                     } | ConvertTo-Json
         Headers = @{
